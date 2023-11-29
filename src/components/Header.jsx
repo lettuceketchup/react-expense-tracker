1ac5a7a79125d7acab79b71ef3a4d07b1ec0930b
Pradhumn Rishishwar <rishishwarmanu@gmail.com>
2023-11-30 02:15:51 +0530

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <h1 className="text-3xl bold">Expense Tracker</h1>;
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'xs': '375px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px'
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
+
